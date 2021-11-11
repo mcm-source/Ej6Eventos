3c3c87ef0df2f7f77907ef8528cb95a0f8d52f60
a20martincm <martinmcm@hotmail.es>
2021-11-11 10:34:29 +0100

    control del tamaño de rotationb

--- app/src/main/java/com/example/ej6eventos/MainActivity.java
+++ app/src/main/java/com/example/ej6eventos/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -23,12 +24,15 @@ public class MainActivity extends AppCompatActivity {
         tvText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                tvText.setRotation(rotation);
+                Log.i("Aviso","Acaba de ejecutarse el método de inicializacion");
+                Log.i("Aviso","Acaba de ejecutarse el método "+String.valueOf(rotation));
                tvText.setText("HOOOOOOLA!!!!!");
                tvText.setBackgroundColor(getResources().getColor(R.color.green));
                tvText.setTextSize(35);
                rotation+=45.0F;
-               if (rotation >=181){
+               if (rotation >=316){
                    rotation=0.0F;
                }
             }
